7c29c3a8f8e123d07f98860d1cb4877bfe21918e
Jorge Garcia Villa <jogar@belikesoftware.com>
2021-05-14 08:35:44 +0200

    actions

--- src/app/modules/sneakers/redux/actions/sneakerActions.ts
+++ src/app/modules/sneakers/redux/actions/sneakerActions.ts
@@ -3,16 +3,26 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
+import { SneakerVO } from "src/app/models/SneakerVO";
 import { AppState } from "src/app/redux/interfaces/app-state";
+import { enviroment } from "src/environments/enviroment.master";
+// @Injectable()
+// export class sneakersActions {
+// static
+//     constructor(
+//       private redux: NgRedux<AppState>,
+//       private http: HttpClient,
+//       private router: Router,
+//       private toastrService: ToastrService
+//     ) { }
 
-@Injectable()
-export class sneakersActions {
-
-    constructor(
-      private redux: NgRedux<AppState>,
-      private http: HttpClient,
-      private router: Router,
-      private toastrService: ToastrService
-    ) { }
+//     createSneaker(sneaker : SneakerVO){
+//       let body = sneaker
+//       this.http.post(enviroment.apiEndPoint,body).subscribe((sneaker)=>{
+//         this.redux.dispatch({
+//           type : 
+//         })
+//       })
+//     }
     
-}
+// }
